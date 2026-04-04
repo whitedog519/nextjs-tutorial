753d3eb7de33465509ede47d750411aa515095a9
Kosuke Takahashi <kousukemac@gmail.com>
2026-04-05 01:10:18 +0900

    loading.tsxで重い処理を非同期化

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import SlowComponent from "./SlowComponent"
 export default async function Home() {
   return (
     <div>
-      <h1>メインコンテンンツ（すぐに表示）</h1>
-      <Suspense fallback={<div>重いコンポーネントを読み込み中...</div>}>
+      {/* <h1>メインコンテンンツ（すぐに表示）</h1> */}
+      {/* <Suspense fallback={<div>重いコンポーネントを読み込み中...</div>}> */}
         <SlowComponent />
-      </Suspense>
+      {/* </Suspense> */}
     </div>
   )
 }
