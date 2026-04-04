f8c630d60f83611df8b622413db717fddb413920
Kosuke Takahashi <kousukemac@gmail.com>
2026-04-05 00:52:22 +0900

    Server Componentsで記事読み込み

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return <h1>Hello World</h1>;
+type Post = {
+  id: number;
+  title: string;
+};
+
+export default async function Home() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts: Post[] = await res.json();
+  return (
+    <div>
+      <h1>記事一覧</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
