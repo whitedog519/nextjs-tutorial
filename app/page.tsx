518d16baa547a2124ba7912c0e2ee9a692804e33
Kosuke Takahashi <kousukemac@gmail.com>
2026-04-08 21:51:45 +0900

    use cache で関数もキャッシュ

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
-export default function Home() {
-  const createAction = async (formData: FormData) => {
-    'use server';
-    const name = formData.get('name');
-    console.log('Server Action で実行されました。')
-  };
+async function getHeavyData() {
+  'use cache'
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return '重いデータの取得完了';
+}
 
-  return <form action={createAction}>
-    <input type="text" name="name" />
-    <button type="submit">送信</button>
-  </form>
+export default async function Home() {
+  const data = await getHeavyData();
+  return (
+    <div>
+      <h1>{data}</h1>
+    </div>
+  )
 }
