84b573c5c78564f4247f1bb80b14112e8aaf1a14
Kosuke Takahashi <kousukemac@gmail.com>
2026-04-09 08:20:07 +0900

    キャッシュ削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { revalidatePath } from "next/cache";
+
 async function getHeavyData() {
   'use cache'
   await new Promise((resolve) => setTimeout(resolve, 3000));
+  revalidatePath('/');
   return '重いデータの取得完了';
 }
 
